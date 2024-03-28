88a909342322ccdcb1f786392c70d367e7950d8a
F_coder <123@123.com>
2024-03-28 11:38:41 +0800

    second commit

---
+++ src/main/java/com/code/walk/WalkApplication.java
@@ -0,0 +1,13 @@
+package com.code.walk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WalkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WalkApplication.class, args);
+    }
+
+}
